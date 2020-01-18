ba19db207fd7055a228be4043b1e68fde3c32630
AllWKa <bryanjaramillobaldeon@gmail.com>
2020-01-18 18:21:42 +0000

    send updates of humidity to telegram

--- server/index.js
+++ server/index.js
@@ -1,6 +1,24 @@
 const SerialPort = require('serialport');
+const TelegramBot = require('node-telegram-bot-api');
+
+const ArduinoBotService = require('./ArduinoBotService')
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const telegramBot = new TelegramBot(process.env.TELEGRAM_BOT_KEY, { polling: true })
+ArduinoBotService.setBot(telegramBot);
 
 const ReadLine = SerialPort.parsers.Readline;
+
+
+telegramBot.onText(/\/myplant/, (msg, match) => {
+    ArduinoBotService.sendMessage(ArduinoBotService.getHumidity() + "%")
+});
+
+telegramBot.onText(/\/start/, (msg, match) => {
+    ArduinoBotService.setChatId(msg.chat.id);
+});
 
 const port = new SerialPort('COM3', {
     baudRate: 9600
@@ -8,13 +26,9 @@ const port = new SerialPort('COM3', {
 
 const parser = port.pipe(new ReadLine({ delimeter: '\r\n' }));
 
-parser.on('open', () => {
-    console.log("connection open");
-});
-
 parser.on('data', (data) => {
-    console.log((100 - (data * 100 / 1023)) + "%");
-})
+    ArduinoBotService.setHumidity((100 - (data * 100 / 1023)));
+});
 
 parser.on('error', (error) => {
     console.log(error);
